6ff382d2bf88411172a6ed7b6f2421dfe4bbab6d
martynenko-e <martynenko.evhen@gmail.com>
2017-07-15 09:58:20 +0300

    Update run.sh

--- run.sh
+++ run.sh
@@ -4,7 +4,6 @@ if [ "$1" == "-b" ]; then
     docker build -t mrtn/startdjango .
 fi
 
-SRC_VOLUME="/Users/yevhenmartynenko/_projects/mrtn_test_temy/backend_src"
+SRC_VOLUME="<your source directory>"
 
-docker run -d -p 8888:8000 --name test_temy -v $SRC_VOLUME:/src mrtn/startdjango
-#docker exec -it blog_site main-admin startproject main
+docker run -d -p 8000:8000 --name test_temy -v $SRC_VOLUME:/src mrtn/startdjango
